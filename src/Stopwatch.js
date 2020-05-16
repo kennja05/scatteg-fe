45aaac65ed659e2d48fbf204f705839a88520fcd
kennja05 <jacobkenny05@gmail.com>
2020-05-16 17:48:32 -0400

    updated rendering of timer to 270 seconds

--- src/Stopwatch.js
+++ src/Stopwatch.js
@@ -6,7 +6,7 @@ export default class Stopwatch extends React.Component {
     render() {
         return (
             <div className='timer'>
-                <Timer initialTime={55000} direction="backward">
+                <Timer lastUnit={'s'} initialTime={270000} direction="backward">
                     {() => (
                         <React.Fragment>
                             <Timer.Seconds /> seconds
