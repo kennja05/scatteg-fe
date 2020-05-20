7dc496b7df0114a2cc662cb924c67bf8f4a400e3
kennja05 <jacobkenny05@gmail.com>
2020-05-20 10:58:41 -0400

    wrote start game function. moved letter array generation out of lifecycle method

--- src/GameContainer.js
+++ src/GameContainer.js
@@ -10,25 +10,37 @@ export default class GameContainer extends React.Component {
         letters: [],
         loaded: false,
         round: '1',
-        list: 0
+        list: 0,
+        gameId: undefined
     }
 
     componentDidMount(){
         fetch('http://localhost:3000/randomlist')
             .then(res => res.json())
-            .then(lists=> this.setState({listArray: lists, loaded: true}))
-            
-        let letterArray = []
-        for (let i = 0; i < 3; i++){
-            let randLetter = this.generateRandomLetter()
-            letterArray.push(randLetter)
-        }    
+            .then(lists=> this.setState({listArray: lists, loaded: true})
+        )   
         this.setState({
-            letters: letterArray
+            letters: this.generateRandomLetters()
         })
+        this.startGame()
+    }
+
+    startGame = () => {
+        const gameObj = {username: 'anonymous'}
+        fetch('http://localhost:3000/games', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            }, 
+            body: JSON.stringify(gameObj)
+        })
+        .then(res => res.json())
+        .then(game => this.setState({gameId: game.id})) //will use this line to place the new game ID into state - hardcoding for now
     }
 
     createRounds = () => {
+        //show letter tells the component to display that rounds letter if we're in the middle of that round or if the round has already been completed
+        //enabled ensures that only the form for the current round is editable
         return ['1', '2', '3'].map(num => <Round showLetter={parseInt(num) <= parseInt(this.state.round)} enabled={this.state.round === num} key={num} roundNumber={num} letter={this.state.letters[parseInt(num)-1]}/>) 
     }
 
@@ -40,19 +52,24 @@ export default class GameContainer extends React.Component {
         })
     }
 
-    generateRandomLetter = () => {
-        const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'W']
-        const randIndex = Math.floor(Math.random() * 21)
-        return letters[randIndex]
+    generateRandomLetters = () => {
+        const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'W'];
+        const myArr = [];
+        for (let i = 0; i < 3; i++) {
+            let randIndex = Math.floor(Math.random() * 21);
+            myArr.push(letters[randIndex])
+        }
+        return myArr
     }
 
     render(){
+        console.log(this.state.gameId)
         return(
             <div className='game-container'>
                 <ListView list={this.state.loaded && this.state.listArray[this.state.list]} loaded={this.state.loaded}/>
                 {/* sets up three separate rounds */}
                 {this.createRounds()} 
-                <Stopwatch incrementRound={this.incrementRound} initialTime={270000}/>
+                {this.state.gameId !== undefined && <Stopwatch incrementRound={this.incrementRound} initialTime={270000}/>}
             </div>
         )
     }
