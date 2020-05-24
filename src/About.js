a9c5821c43c9912dfb97e522f302e195a9f3e1de
kennja05 <jacobkenny05@gmail.com>
2020-05-24 13:42:04 -0400

    added about section

---
+++ src/About.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const About = () => {
+    return(
+        <div>
+            <p>
+                Scattegories is traditionally played as a party game in which players compete to 
+                uniquely name objects that fit within the various categories prescribed by the game.
+            </p>
+            <p>
+                Although there are various similar games, the Scattegories that we all know today was
+                originaly published in 1988 by the Parker Brothers company which is now owned by
+                Hasbro. 
+            </p>
+            <p>
+                The goal of this online Scattegories is very similar to the original board game with
+                a few obvious differences. First, the user must play the three rounds of their game 
+                consecutively, without any break in between rounds. Second, the user will not score 
+                their own games. Once the user has completed their game, it will fall on to the other
+                users to grade/score the game. The top scoring users will appear on a leaderboard for
+                bragging rights.
+            </p>
+        </div>
+    )
+}
+
+export default About 
--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Route, Switch } from 'react-router-dom'
 import './App.css';
 
 import GameContainer from './GameContainer'
+import About from './About'
 
 function App() {
   return (
     <div className="App">
       <Switch>
-        <Route path='/' render={() => <GameContainer />} /> 
+        <Route exact path='/' render={() => <GameContainer />} /> 
+        <Route path = '/about' component={About} />
       </Switch>
     </div>
   );
